163dc3c2026c4df6ebded5752d6635750875eca7
CComps <99871804+CComps@users.noreply.github.com>
2023-05-20 19:38:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,6 @@ try:
     nltk.download("punkt")
     nltk.download("wordnet")
     nltk.download("omw-1.4")
-    nltk.download("stopwords")
-    nltk.download("averaged_perceptron_tagger")
     nltk.download("*")
 except:
     pass
